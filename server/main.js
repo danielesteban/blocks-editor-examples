23737ffb7b8ef446c86f8b8fa0c321c1d87792d0
Daniel Esteban Nombela <dani@gatunes.com>
2020-11-08 08:45:24 +0100

    Small p2p signaling server for multiplayer worlds

---
+++ server/main.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const expressWS = require('express-ws');
+const helmet = require('helmet');
+const Room = require('./room');
+
+const rooms = [
+  'Climb',
+  'Island',
+].map((id) => new Room(id));
+
+const server = express();
+server.use(helmet());
+expressWS(server, null, { clientTracking: false });
+rooms.forEach((room) => {
+  server.ws(`/:${room.id}`, room.onClient.bind(room));
+});
+server.use((req, res) => res.status(404).end());
+server.listen(process.env.PORT || 3000);
---
+++ server/room.js
@@ -0,0 +1,133 @@
+const { v4: uuid } = require('uuid');
+
+class Room {
+  constructor(id) {
+    this.id = id;
+    this.clients = [];
+    this.maxClients = process.env.MAX_CLIENTS ? parseInt(process.env.MAX_CLIENTS, 10) : 16;
+  }
+
+  onClose(client) {
+    const { clients, pingInterval } = this;
+    const index = clients.findIndex(({ id }) => (id === client.id));
+    if (~index) {
+      clients.splice(index, 1);
+      this.broadcast({
+        type: 'LEAVE',
+        data: client.id,
+      });
+      if (!clients.length && pingInterval) {
+        clearInterval(pingInterval);
+        delete this.pingInterval;
+      }
+    }
+  }
+
+  onClient(client) {
+    const { clients, maxClients, pingInterval } = this;
+    if (clients.length >= maxClients) {
+      client.send(JSON.stringify({
+        type: 'ERROR',
+        data: 'Room is full. Try another room.',
+      }), () => {});
+      client.terminate();
+      return;
+    }
+    client.id = uuid();
+    client.send(JSON.stringify({
+      type: 'INIT',
+      data: {
+        peers: clients.map(({ id }) => (id)),
+        ...(this.onInit ? this.onInit(client) : {}),
+      },
+    }), () => {});
+    this.broadcast({
+      type: 'JOIN',
+      data: client.id,
+    });
+    clients.push(client);
+    client.isAlive = true;
+    client.once('close', () => this.onClose(client));
+    client.on('message', (data) => this.onMessage(client, data));
+    client.on('pong', () => {
+      client.isAlive = true;
+    });
+    if (!pingInterval) {
+      this.pingInterval = setInterval(this.ping.bind(this), 60000);
+    }
+  }
+
+  onMessage(client, data) {
+    let request;
+    try {
+      request = JSON.parse(data);
+    } catch (e) {
+      return;
+    }
+    this.onRequest(client, request);
+  }
+
+  onRequest(client, request) {
+    const { clients } = this;
+    switch (request.type) {
+      case 'SIGNAL': {
+        let { peer, signal } = request.data;
+        peer = `${peer}`;
+        signal = `${signal}`;
+        if (!(
+          !peer
+          || !signal
+          || clients.findIndex(({ id }) => (id === peer)) === -1
+        )) {
+          if (client) {
+            this.broadcast({
+              type: 'SIGNAL',
+              data: {
+                peer: client.id,
+                signal,
+              },
+            }, {
+              include: peer,
+            });
+          }
+        }
+        break;
+      }
+      default:
+        break;
+    }
+  }
+
+  broadcast(event, { exclude, include } = {}) {
+    const { clients } = this;
+    const encoded = JSON.stringify(event);
+    if (exclude && !Array.isArray(exclude)) {
+      exclude = [exclude];
+    }
+    if (include && !Array.isArray(include)) {
+      include = [include];
+    }
+    clients.forEach((client) => {
+      if (
+        (!include || ~include.indexOf(client.id))
+        && (!exclude || exclude.indexOf(client.id) === -1)
+      ) {
+        client.send(encoded, () => {});
+      }
+    });
+  }
+
+  ping() {
+    const { clients } = this;
+    clients.forEach((client) => {
+      if (client.isAlive === false) {
+        client.terminate();
+        return;
+      }
+      client.isAlive = false;
+      client.ping(() => {});
+    });
+  }
+}
+
+module.exports = Room;
